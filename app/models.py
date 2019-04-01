a1a53be389e716ca21afc1801202e7799e64eff2
ibeckermayer <ibeckermayer@gmail.com>
2019-04-01 15:47:54 -0700

    added setup for sqlalchemy

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(64), index=True, unique=False)
+    last_name = db.Column(db.String(64), index=True, unique=False)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))

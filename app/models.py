f49898a339a8a46ffed29631e838c6951e1b655d
ibeckermayer <ibeckermayer@gmail.com>
2019-04-01 23:26:35 -0700

    getting started with the login manager section of the tutorial

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(64), index=True, unique=False)
     last_name = db.Column(db.String(64), index=True, unique=False)
@@ -13,3 +15,7 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))

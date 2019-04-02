4bc76d1b21c0a20e7f834fafbcb04c7c9ae29863
ibeckermayer <ibeckermayer@gmail.com>
2019-04-02 16:39:23 -0700

    added comments

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
+app.config.from_object(Config)  # configure app from Config obj
+db = SQLAlchemy(app)            # initialize database for app
+migrate = Migrate(app, db)      # initialize migration manager for app + db
+login_manager = LoginManager(app)
+login_manager.init_app(app)     # initialize login manager
 
 from app import routes, models

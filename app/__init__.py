bd947d453dc3005d439384fca53a365bf15129e5
ibeckermayer <ibeckermayer@gmail.com>
2019-04-02 18:07:17 -0700

    deleted redundant init_app for login manager

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 app.config.from_object(Config)  # configure app from Config obj
 db = SQLAlchemy(app)            # initialize database for app
 migrate = Migrate(app, db)      # initialize migration manager for app + db
-login_manager = LoginManager(app)
-login_manager.init_app(app)     # initialize login manager
+login_manager = LoginManager(app)  # initialize login manager
 
 from app import routes, models

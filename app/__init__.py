e4bd06b768a3d9cad8f201ea0e753afd28533592
ibeckermayer <ibeckermayer@gmail.com>
2019-04-05 14:57:55 -0700

    took out login manager and deleted routes, to be replaced by resources

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,11 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config.from_object(Config)  # configure app from Config obj
 db = SQLAlchemy(app)            # initialize database for app
 migrate = Migrate(app, db)      # initialize migration manager for app + db
-login_manager = LoginManager(app)  # initialize login manager
 
 from app import routes, models

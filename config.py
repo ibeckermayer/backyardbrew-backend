273dc0c751895412aa64b247147b02fdfc1164e1
ibeckermayer <ibeckermayer@gmail.com>
2019-04-08 20:10:21 -0700

    changed flaskenv to have FLASK_DEBUG variable, changed db name in config to debug.db, ignoring *.db in gitignore, added base test class

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class ProductionConfig(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'debug.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 

36defedf47fd1dd0f64216dfd49350cc56eb6279
ibeckermayer <ibeckermayer@gmail.com>
2019-04-08 19:34:03 -0700

    added ProductionConfig vs TestConfig classes

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
+
+class ProductionConfig(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestConfig(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    TESTING = True

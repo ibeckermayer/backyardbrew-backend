ab49d3e26a43ee58c75f2ea35ccd00f281045a68
ibeckermayer <ibeckermayer@gmail.com>
2019-04-10 18:23:11 -0700

    adding server name to config

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config(object):
     SECRET_KEY = os.getenv('SECRET')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SERVER_NAME = "127.0.0.1:5000"
 
 
 class DevelopmentConfig(Config):
@@ -20,6 +21,7 @@ class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/backyardbrew_test_db'
     DEBUG = True
+
 
 class StagingConfig(Config):
     """Configurations for Staging."""

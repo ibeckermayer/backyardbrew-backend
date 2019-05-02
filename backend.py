7c3d676db117cf5abafd4f6c9a2f5f7eb0afa714
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2019-05-02 11:50:55 -0700

    adding functions to manipulate database from flask shell

--- backend.py
+++ backend.py
@@ -1,5 +1,19 @@
 import os
-from app import create_app
+from app import create_app, db
+from flask_sqlalchemy import SQLAlchemy
 
 config_name = os.getenv('APP_SETTINGS')  # config_name = "development"
 app = create_app(config_name)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'reset_db': reset_db}
+
+
+def reset_db(db: SQLAlchemy):
+    '''
+    Function to reset the database from the flask shell.
+    '''
+    db.drop_all()
+    db.create_all()

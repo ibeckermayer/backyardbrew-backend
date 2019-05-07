1e2c3e0e5fb953ddbc20c7c92852e4f625fce668
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2019-05-07 15:25:52 -0700

    added category_data to each item and returned list of categories in the fullcatalog endpoint

--- app/resources.py
+++ app/resources.py
@@ -10,6 +10,9 @@ from app.util import (add_token_to_database, is_token_revoked, revoke_token)
 import squareconnect
 from squareconnect.apis.catalog_api import CatalogApi
 from squareconnect.models import SearchCatalogObjectsRequest
+from squareconnect.models.catalog_object import CatalogObject
+from squareconnect.models.catalog_item import CatalogItem
+from typing import List
 from config import SQUARE_ACCESS_TOKEN
 
 
@@ -149,26 +152,52 @@ class FeedbackEndpoint(Resource):
 class FullCatalog(Resource):
     '''
     NOTE: Super-annoying-ly, the api doesn't automatically return link ITEM catalog objects to their respective IMAGE
-    This endpoint is built to grab all the ITEM's and IMAGE's, associate all the ITEM objects with their corresponding IMAGE
-    and then return the ITEM list. The frontend can then store/sort/display this data.
+    This endpoint is built to grab all the ITEM's and IMAGE's, associate all the ITEM objects (copy image url in ITEM's
+    image_url field) with their corresponding IMAGE and then return the ITEM list. The frontend can then store/sort/display
+    this data.
+
+    The endpoint ultimately returns a field called 'items' that is a List[squareconnect.models.catalog_item.CatalogItem].
+    I modify the CatalogItem object to contain a new string field called category_data that contains the data from that
+    field in its corresponding squareconnect.models.catalog_category.CatalogCategory.
+
+    Finally the endpoint returns a field called 'catgories' which is a List[CatalogCategory] for
+    use by the frontend.
     '''
 
     def get(self):
         api_instance = CatalogApi()
         api_instance.api_client.configuration.access_token = SQUARE_ACCESS_TOKEN
         body = SearchCatalogObjectsRequest(
-            object_types=["ITEM", "IMAGE"])  # specify all ITEM and IMAGE
-        items_and_images = api_instance.search_catalog_objects(
-            body=body).objects
-        items = self.associate_item_with_image_url(
-            list(filter(lambda obj: obj.type == 'ITEM', items_and_images)),
-            list(filter(lambda obj: obj.type == 'IMAGE', items_and_images)))
+            object_types=['ITEM', 'IMAGE',
+                          'CATEGORY'])  # specify all ITEM, IMAGE, and CATEGORY
+        itms_imgs_and_cats = api_instance.search_catalog_objects(
+            body=body
+        ).objects  # List[squareconnect.models.catalog_object.CatalogObject]
+        itms_only = list(
+            filter(lambda obj: obj.type == 'ITEM', itms_imgs_and_cats))
+        imgs_only = list(
+            filter(lambda obj: obj.type == 'IMAGE', itms_imgs_and_cats))
+        cats_only = list(
+            filter(lambda obj: obj.type == 'CATEGORY', itms_imgs_and_cats))
+        itms_w_imgs = self.__associate_item_data_with_image_data_url(
+            itms_only, imgs_only)
+        itms_w_imgs_w_cats = self.__associate_item_data_with_category_data(
+            itms_w_imgs, cats_only)
         return {
-            'msg': 'Items retrieved',
-            'items': [item.item_data.to_dict() for item in items]
+            'msg':
+            'Items retrieved',
+            'items': [
+                self.__catalog_item_to_dict(item.item_data)
+                for item in itms_w_imgs_w_cats
+            ],  # List[CatalogItem] w/ added category_data attr
+            'categories': [cat.category_data.to_dict()
+                           for cat in cats_only]  # List[CatalogCategory]
         }
 
-    def associate_item_with_image_url(self, items, images):
+    def __associate_item_data_with_image_data_url(self,
+                                                  items: List[CatalogObject],
+                                                  images: List[CatalogObject]
+                                                  ) -> List[CatalogObject]:
         '''
         because the square API doesn't gracefully do this for us, we manually fill out each item's
         item_data.image_url with appropriate image url
@@ -179,6 +208,27 @@ class FullCatalog(Resource):
                     item.item_data.image_url = image.image_data.url
                     break
         return items
+
+    def __associate_item_data_with_category_data(
+            self, items: List[CatalogObject],
+            categories: List[CatalogObject]) -> List[CatalogObject]:
+        '''
+        create new item.item_data field called category_data to associate each item with its category
+        '''
+        for item in items:
+            for category in categories:
+                if (item.item_data.category_id == category.id):
+                    item.item_data.category_data = category.category_data
+                    break
+        return items
+
+    def __catalog_item_to_dict(self, citem: CatalogItem) -> dict:
+        '''
+        default to_dict function for CatalogItem doesn't capture our added category_data field, so we add it to the dictionary manually
+        '''
+        d = citem.to_dict()
+        d['category_data'] = citem.category_data.to_dict()
+        return d
 
 
 # Define our callback function to check if a token has been revoked or not
--- test/test_fullcatalog.py
+++ test/test_fullcatalog.py
@@ -7,12 +7,14 @@ ENDPOINT = 'api/fullcatalog'
 
 
 def test_all_item(testing_client: FlaskClient, testing_db: SQLAlchemy):
-    # set body to ask api for all items
-    data = json.dumps({"body": {"object_types": ["ITEM"]}})
-
     response = testing_client.get(ENDPOINT)
     response_json = json.loads(response.data)
     assert response.status_code == 200
     assert response_json['msg'] == 'Items retrieved'
     for item in response_json['items']:
-        assert item['image_url'] != None
+        assert item[
+            'image_url'] != None  # check that all items have an associated image_url
+        assert item[
+            'category_data'] != None  # check that all items have an associated category_data
+    assert 'Coffee' in [cat['name'] for cat in response_json['categories']
+                        ]  # check that Coffee is one of the categories

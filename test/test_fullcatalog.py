5a83be4add7f308014af0413b4b9e108cf952f3b
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2019-05-10 14:08:57 -0700

    ensuring tax id's are returned for each item in test

--- test/test_fullcatalog.py
+++ test/test_fullcatalog.py
@@ -18,6 +18,8 @@ def test_all_item(testing_client: FlaskClient, testing_db: SQLAlchemy):
             'image_url'] != None  # check that all items have an associated image_url
         assert item[
             'category_data'] != None  # check that all items have an associated category_data
-        assert item['category_data']['name'] != None
+        assert item['category_data'][
+            'name'] != None  # check all items have a category name
+        assert len(item['tax_ids']) != 0  # check items have tax id
     for cat in response_json['categories']:
         assert cat['name'] in CATEGORIES
